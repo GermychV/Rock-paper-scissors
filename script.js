d2f8223fc45854465193b0134ecc150ab9da8a44
GermychV <132744771+GermychV@users.noreply.github.com>
2025-08-25 15:18:20 +0200

    don

--- script.js
+++ script.js
@@ -68,4 +68,11 @@ function playGame() {
     
 }
 
-playGame()
+// playGame()
+
+function sumOfTripledEvens(array) {
+  return array
+    .filter((num) => num % 2 === 0)
+    .map((num) => num * 3)
+    .reduce((acc, curr) => acc + curr)
+}
